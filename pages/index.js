ce17897d200178338f0a38d0dbfcc59ee1ce5bf0
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-30 13:51:12 +0800

    Add post lists

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Message from '@/components/Message'
+import { useEffect, useState } from 'react'
+import { db } from '@/utils/firebase'
+import { query, collection, onSnapshot, orderBy } from 'firebase/firestore'
 
 export default function Home() {
+  const [allPosts, setAllPosts] = useState([])
+
+  const getPosts = async () => {
+    const collRef = collection(db, 'posts')
+    const q = query(collRef, orderBy('timestamp', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
+    })
+    return unsubscribe
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
       <Head>
@@ -12,7 +28,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <div className='my-12 text-lg font-medium'>
+        <h2>See what other people are saying</h2>
+        {allPosts.map(post => (
+          <Message {...post}></Message>
+        ))}
+      </div>
     </>
   )
 }
