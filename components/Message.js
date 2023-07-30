1c0126d7130f4d9fdb17e068942473e258da22ed
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-30 17:19:35 +0800

    Edit styling

--- components/Message.js
+++ components/Message.js
@@ -1,11 +1,11 @@
 export default function Message({ children, avatar, username, description }) {
     return (
-        <div className='bg-white p-8 border-b-2 rounded-lg'>
+        <div className='bg-white p-8 mt-2 shadow rounded-lg'>
             <div className='flex items-center gap-4'>
                 <img src={avatar} className='w-10 rounded-full' />
-                <h2>{username}</h2>
+                <h2 className="font-semibold">{username}</h2>
             </div>
-            <div className='py-4'>
+            <div className='py-4 text-base font-normal'>
                 <p>{description}</p>
             </div>
             {children}
